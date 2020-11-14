3a5d38bd87b5de8f782eef8217f69effffdc7c68
bngom <barthe.ngom@gmail.com>
2020-11-14 21:51:25 +0100

    automate gitlab-runner registration

--- iac/user_data.sh
+++ iac/user_data.sh
@@ -2,18 +2,18 @@
 sudo apt update -y
 sudo apt upgrade -y
 
+# Linux x86-64
+sudo wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+sudo chmod +x /usr/local/bin/gitlab-runner
+
 # Install docker
-sudo apt install docker.io -y
+curl -sSL https://get.docker.com/ | sh
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Linux x86-64
-sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
-
-sudo chmod +x /usr/local/bin/gitlab-runner
 
 sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 
@@ -23,8 +23,12 @@ sudo gitlab-runner start
 
 # Register Gitlab-runner
 # sudo gitlab-runner register
-sudo gitlab-runner register -n --url https://gitlab.com/ --registration-token HRgyhyEgV_cfWuVdv8bB --executor shell --description "My Runner"
+sudo gitlab-runner register -n --url https://gitlab.com/ --registration-token RbgvFxTcz_P4FfxGyz4J --tag-list uat --executor shell --description "My Runner"
 
-# sudo usermod -aG docker gitlab-runner
+sudo usermod -aG docker gitlab-runner
+
+echo 'gitlab-runner ALL=(ALL) NOPASSWD: ALL' >> sudo /etc/sudoers
+
+sudo gitlab-runner restart
 
 
