a761a1ac228f2a5a71771b0a1735e68946a17f87
bngom <barthe.ngom@gmail.com>
2020-11-15 17:52:20 +0100

    update bootstrap script

--- iac/user_data.sh
+++ iac/user_data.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
-# This script bootstraps the ec2 in such a way almost everything is setup to start a new yolo run on docker
-# This script has been tested on an EC2 T2 MICRO
+
+# Update and upgrade packages
 sudo apt update -y
 sudo apt upgrade -y
 
+# Linux x86-64
+sudo wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+sudo chmod +x /usr/local/bin/gitlab-runner
+
 # Install docker
-sudo apt install docker.io -y
+sudo curl -sSL https://get.docker.com/ | sh
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Add GitLab’s official repository
-sudo curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+# Add user gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 
-# Install the latest version of GitLab Runner
-export GITLAB_RUNNER_DISABLE_SKEL=true; sudo -E apt-get install gitlab-runner
+# Install gitlab-runner
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 
-# Register Gitlab-runner
-# sudo gitlab-runner register
+# Start gitlab-runner
+sudo gitlab-runner start
 
-# Give it permissions to execute
-# sudo chmod +x /usr/local/bin/gitlab-runner
+# Register gitlab-runner
+sudo gitlab-runner register -n \
+  --url https://gitlab.com/ \
+  --registration-token RbgvFxTcz_P4FfxGyz4J \
+  --executor shell \
+  --description "My Runner"
 
-# Create a GitLab CI user:
-# sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo usermod -aG docker gitlab-runner
+
+# sudo echo 'gitlab-runner ALL=(ALL) NOPASSWD: ALL' >> sudo /etc/sudoers
+
+# sudo gitlab-runner restart
+
+
